fc20b96d43c9e2312ca90877885f7d61274ffbf1
16Yongjin <yongjin0802@gmail.com>
2021-01-15 02:10:04 +0900

    reset cache

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,4 +1,4 @@
-import { cacheExchange, Resolver } from '@urql/exchange-graphcache'
+import { cacheExchange, Resolver, Cache } from '@urql/exchange-graphcache'
 import {
   LoginMutation,
   MeQuery,
@@ -67,6 +67,14 @@ export const cursorPagination = (): Resolver => {
   }
 }
 
+const invalidateAllPosts = (cache: Cache) => {
+  const allFields = cache.inspectFields('Query')
+  const fieldInfos = allFields.filter((info) => info.fieldName === 'posts')
+  fieldInfos.forEach((fi) => {
+    cache.invalidate('Query', 'posts', fi.arguments || {})
+  })
+}
+
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = ''
   if (isServer()) {
@@ -128,13 +136,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
               }
             },
             createPost: (_result, _args, cache, _info) => {
-              const allFields = cache.inspectFields('Query')
-              const fieldInfos = allFields.filter(
-                (info) => info.fieldName === 'posts'
-              )
-              fieldInfos.forEach((fi) => {
-                cache.invalidate('Query', 'posts', fi.arguments || {})
-              })
+              invalidateAllPosts(cache)
             },
             login: (_result, _args, cache, _info) => {
               betterUpdateQuery<LoginMutation, MeQuery>(
@@ -151,6 +153,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
                   }
                 }
               )
+              invalidateAllPosts(cache)
             },
             register: (_result, _args, cache, _info) => {
               betterUpdateQuery<RegisterMutation, MeQuery>(
